5e3fc29663e3de1de0d2cbbfbe4232753e5687c8
Aartisushmesh29 <134151553+Aartisushmesh29@users.noreply.github.com>
2024-03-17 14:15:16 +0000

    second commit after second file creation.

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";   // treat all js code as its modern/newer version
+//  alert(3+3)  // we are using node js , not browser 
+console.log(3+3)
+
+/* DATA TYPES
+  1. Number => 2 to power 53
+  2. bigint 
+  3. string => ""
+  4. boolean  => true/false
+  5. null => standalone value
+  6. undefined => 
+  7.symbol => unique
+
+
+  Object 
+
+  */
+
+  console.log(typeof "hitesh");
+  console.log(typeof age);
+  console.log(typeof null);  //object
+  console.log(typeof undefined); //undefined
---
+++ 01_basics/first.js
@@ -0,0 +1,21 @@
+console.log("hitesh")
+
+const accountId = 144553
+let accountEmail = "aarti@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// In JS variable can be declared without any  keyword like accountCIty 
+
+// accouontId = 2 --> not allowd as it is const declared
+
+accountEmail= "hc@hc.com"
+accountPassword = "2131313"
+accountCity = "Amroha"
+
+/* it is not preferred in js to declare a variable wiht "var" keyword 
+bacause of the issue of block scope and functional scope
+*/
+ console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword, accountCity])
