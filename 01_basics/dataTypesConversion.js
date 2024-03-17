778524c887ee0c0b5bbc367d3f7e9db1a6007410
Aartisushmesh29 <134151553+Aartisushmesh29@users.noreply.github.com>
2024-03-17 15:17:34 +0000

    conversion  of datatypes

---
+++ 01_basics/dataTypesConversion.js
@@ -0,0 +1,32 @@
+// let score = "33"           // =>33
+// let score = "33abc"     =>// NaN
+// let score = null         // =>0
+// let score = undefined   //=>// NaN
+// let score = "hitesh"   //=> NaN
+// let score = true      // bool
+
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "hitesh"
+// let booleannIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleannIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false 
+// "hitesh"=>true
+
+let someNumber = 33 
+
+let stringNumber = String (someNumber) 
+console.log(stringNumber);
+console.log( typeof stringNumber);
