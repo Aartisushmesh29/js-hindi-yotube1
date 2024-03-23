178a3222e95b2382d47e91453c55e0d0f8f1f974
Aartisushmesh29 <134151553+Aartisushmesh29@users.noreply.github.com>
2024-03-23 16:49:16 +0000

    DataTypes Summary

--- 01_basics/DTsummary.js
+++ 01_basics/DTsummary.js
@@ -7,11 +7,30 @@ Reference (Non primitive)
 Array , Objects, Functions 
 */
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
 
-const bigNumber = 1344546476876895n
+// const bigNumber = 1344546476876895n
 
+// stack(primitive) , heap (Non-primitive )
+
+let myYoutubename ="hiteshchoudhary"
+/* after assigning the first variable to another it will not change the value of first variable as copy of first variable is passed here (stack memory)
+*/
+let anotherName  = myYoutubename;
+anotherName = "aarti"
+console.log(myYoutubename)
+console.log(anotherName)
+
+let userOne = {
+    email: " user@google.com",
+    upi : "usesr@ybl"
+}
+// after assigning the value to another variable value of boject will get vahnge as reference is passed(heapp memeory)
+let userTwo = userOne 
+userTwo.email = "hitesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
